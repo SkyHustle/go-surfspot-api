d9721b580591ca7eb8fac57ecccbbf8e4d85f8cf
Dmitry Sky <dmitry.sky@icloud.com>
2020-08-19 13:37:45 -1000

    Create a data store, an explicit get method and a constructor for surfspot

--- server.go
+++ server.go
@@ -13,16 +13,29 @@ type Surfspot struct {
 	Difficulty int
 }
 
-// surfspotsHandler handles http request and response
-func surfspotsHandler(w http.ResponseWriter, r *http.Request) {
+// Temporary data store
+type surfspotHandlers struct {
+	store map[string]Surfspot
+}
+
+// surfspotHandlers handles http request and response
+func (h *surfspotHandlers) get(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("w \n", w)
 	fmt.Println("r \n", r)
 	fmt.Printf("w is of type %T \n and r is of type %T ", w, r)
 }
 
+func newSurfspotHandlers() *surfspotHandlers {
+	return &surfspotHandlers{
+		store: map[string]Surfspot{},
+	}
+}
+
 func main() {
-	// HandleFunc registers surfspotsHandler for "/surfspots"
-	http.HandleFunc("/surfspots", surfspotsHandler)
+	surfspotHandlers := newSurfspotHandlers()
+	fmt.Println(surfspotHandlers)
+	// HandleFunc registers surfspotHandlers for "/surfspots"
+	http.HandleFunc("/surfspots", surfspotHandlers.get)
 
 	// Simple http server that takes a port and a default handler
 	err := http.ListenAndServe(":8080", nil)
