1b0698e09e08bf4447769f57c02837a7f9e025a5
Dmitry Sky <dmitry.sky@icloud.com>
2020-08-19 11:47:59 -1000

    Create a Get route for surfspots

--- server.go
+++ server.go
@@ -1,8 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+func surfspotsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("w \n", w)
+	fmt.Println("r \n", r)
+	fmt.Printf("w is of type %T \n and r is of type %T ", w, r)
+}
 
 func main() {
+	http.HandleFunc("/surfspots", surfspotsHandler)
+
 	// Simple http server that takes a port and a default handler
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
